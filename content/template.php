42a83927bf519e55a95b8ef1ef4b64eb391d2352
James Oswald <jamesoswald111@gmail.com>
2018-03-07 14:33:06 -0500

    Add files via upload

---
+++ content/template.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php include "../res/meta.html"?>
+		<title> Page Title Here </title>
+		<!-- Your CSS links and JS libraries here -->
+	</head>
+	<body>
+		<?php include "../res/sides.html"?>
+		<?php include "../res/header.html"?>
+		<div id="content">
+			
+		</div>
+	</body>
+</html>
