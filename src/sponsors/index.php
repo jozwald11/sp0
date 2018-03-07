8a4931904a68b6365d278246473a6c3b32fd6d98
James Oswald <jamesoswald111@gmail.com>
2018-03-07 14:26:44 -0500

    Add files via upload

---
+++ src/sponsors/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php include "../res/meta.html"?>
+		<link rel="stylesheet" type="text/css" href="/sponsors/index.css"/>
+		<title> Orange Blinking Lights </title>
+	</head>
+	<body>
+		<?php include "../res/sides.html"?>
+		<?php include "../res/header.html"?>
+		<div id="content">
+			<p> 
+				Our team receives no school funding, Instead we rely on the community and generous individuals to help
+				us pay for our expenses. We are a non-profit organazation and all donations and sponsorships will go to
+				helping our team pay for robot parts, transportation, and FRC entry fees.<br/>
+				<br/> 
+				You can donate to us via 
+				<a href="../contact/">contacting us</a> or at 
+				<a href="https://www.gofundme.com/frcteam6969"> our GoFundMe Page </a>.
+			</p>
+			<iframe src="https://www.gofundme.com/frcteam6969">
+				Your browser fails to support iframes but use the link to get to our gofundme page.
+			</iframe>
+		</div>
+	</body>
+</html>
