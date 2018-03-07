2cd6fb48b18b7a2dc7f20397711c030858083688
James Oswald <jamesoswald111@gmail.com>
2018-03-07 14:21:38 -0500

    Add files via upload

---
+++ src/contact/index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php include "../res/meta.html"?>
+		<link rel="stylesheet" type="text/css" href="/contact/index.css"/>
+		<title> Orange Blinking Lights </title>
+	</head>
+	<body>
+		<?php include "../res/sides.html"?>
+		<?php include "../res/header.html"?>
+		<div id="content">
+			<?php include "../res/discord.html"?>
+			<h1> Contacts </h1>
+			<p>
+				For general contact use our team email: <b>orangeblinkinglights@gmail.com</b><br/>
+				For more specific questions contact one of our admins or Captains.<br/>
+				You can also contact us on Discord a at <a href="https://discord.gg/jf4nAHp">https://discord.gg/jf4nAHp</a>
+			</p>
+			<h3> Admins </h3>
+			<table class="tg">
+			  <tr>
+				<th class="tg-9hbo">Name</th>
+				<th class="tg-yw4l">Role</th>
+				<th class="tg-yw4l">Email</th>
+				<th class="tg-yw4l">Number</th>
+			  </tr>
+			  <tr>
+				<td class="tg-yw4l">Zekai Zheng</td>
+				<td class="tg-yw4l">President</td>
+				<td class="tg-yw4l">zekaiz123@gmail.com</td>
+				<td class="tg-yw4l">845-671-1466</td>
+			  </tr>
+			  <tr>
+				<td class="tg-yw4l">Michaela Mazis</td>
+				<td class="tg-yw4l">Vice President</td>
+				<td class="tg-yw4l">mmazis14@gmail.com</td>
+				<td class="tg-yw4l">845-558-4865</td>
+			  </tr>
+			  <tr>
+				<td class="tg-yw4l">Kayleigh Ross</td>
+				<td class="tg-yw4l">Treasurer</td>
+				<td class="tg-yw4l">Kayleighr242@gmail.com</td>
+				<td class="tg-yw4l">845-248-7289</td>
+			  </tr>
+			  <tr>
+				<td class="tg-yw4l">James Oswald</td>
+				<td class="tg-yw4l">Server Admin<br>Webmaster</td>
+				<td class="tg-yw4l">jamesoswald111@gmail.com</td>
+				<td class="tg-yw4l">845-671-2647</td>
+			  </tr>
+			  <tr>
+				<td class="tg-yw4l">Peter Zollinger</td>
+				<td class="tg-yw4l">Mentor</td>
+				<td class="tg-yw4l">pjzollinger@wpi.edu</td>
+				<td class="tg-yw4l">845-269-9204</td>
+			  </tr>
+			  <tr>
+				<td class="tg-yw4l">Alexander Lam</td>
+				<td class="tg-yw4l">Mentor</td>
+				<td class="tg-yw4l">alexlam@mit.edu</td>
+				<td class="tg-yw4l">845-263-2717</td>
+			  </tr>
+			</table>
+			<h3> Captains </h3>
+			<table class="tg">
+				<tr>
+					<th class="tg-8fc1">Name</th>
+					<th class="tg-h31u">Role</th>
+					<th class="tg-h31u">Email</th>
+					<th class="tg-h31u">Number</th>
+				</tr>
+				<tr>
+					<td class="tg-h31u">Jay Cruz</td>
+					<td class="tg-h31u">Mechanical Captain</td>
+					<td class="tg-h31u" colspan="2">jayesc14@gmail.com</td>
+				</tr>
+				<tr>
+					<td class="tg-h31u">Michael Schutzman</td>
+					<td class="tg-h31u">Electrical Captain</td>
+					<td class="tg-h31u">michaelschutzman@hotmail.com</td>
+					<td class="tg-h31u">845-705-5026</td>
+				</tr>
+				<tr>
+					<td class="tg-h31u">James Oswald</td>
+					<td class="tg-h31u">Programming Captain</td>
+					<td class="tg-h31u">jamesoswald111@gmail.com</td>
+					<td class="tg-h31u">845-671-2647</td>
+				</tr>
+				<tr>
+					<td class="tg-h31u">Wyatt Mattaway</td>
+					<td class="tg-h31u">Writing Captain</td>
+					<td class="tg-h31u">wmattaway@gmail.com</td>
+					<td class="tg-h31u">845-422-2792</td>
+				</tr>
+				<tr>
+					<td class="tg-h31u">Kayleigh Ross</td>
+					<td class="tg-h31u">Finance Captain</td>
+					<td class="tg-h31u">Kayleighr242@gmail.com</td>
+					<td class="tg-h31u">845-248-7289</td>
+				</tr>
+				<tr>
+					<td class="tg-yw4l">Carrington Gregori</td>
+					<td class="tg-yw4l">Public Relations Captain</td>
+					<td class="tg-yw4l" colspan="2">cgregori202@gmail.com</td>
+				</tr>
+				<tr>
+					<td class="tg-h31u">Jarius Thomas</td>
+					<td class="tg-h31u">Spirit Capain</td>
+					<td class="tg-h31u" colspan="2">JetStreakMusic@gmail.com</td>
+				</tr>
+				<tr>
+					<td class="tg-yw4l">Joseph Jang</td>
+					<td class="tg-yw4l">Safety Captain</td>
+					<td class="tg-yw4l" colspan="2">ilo99rl@gmail.com</td>
+				</tr>
+				<tr>
+					<td class="tg-yw4l">Michael Sluchevsky</td>
+					<td class="tg-yw4l">Art Captain</td>
+					<td class="tg-yw4l" colspan="2">michaelsluchevsky@gmail.com</td>
+				</tr>
+				<tr>
+					<td class="tg-yw4l">Liam Tracey</td>
+					<td class="tg-yw4l" colspan="3">Carpentry Captain</td>
+				</tr>
+				</table>
+		</div>
+	</body>
+</html>
