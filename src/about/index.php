6de597fcfa94b49d8f25d1c7a80d57bdef245ce0
James Oswald <jamesoswald111@gmail.com>
2018-03-07 14:21:05 -0500

    Add files via upload

---
+++ src/about/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php include "../res/meta.html"?>
+		<link rel="stylesheet" type="text/css" href="/about/index.css"/>
+		<title> Orange Blinking Lights </title>
+	</head>
+	<body>
+		<?php include "../res/sides.html"?>
+		<?php include "../res/header.html"?>
+		<div id="content">
+			<?php include "../res/discord.html"?>
+			<h2>Who Are We?</h2>
+			<p>
+				The Orange Blinking Lights are Clarkstown South High School’s robotics club.<br/>
+				We are a rookie team in the 2017 FIRST Robotics Competition (Team #6969).<br/>
+			</p>
+			<img id="india" src="/res/img/FreshOrient.jpg"/>
+			<h2>What We Do:</h2>
+			<p>
+				Our goal is to encourage students with different backgrounds to explore the<br/>
+				process of collaboration in a technology-centric environment. As members of<br/>
+				The Orange Blinking Lights establish individual roles, they also develop<br/>
+				skills that will be valuable in STEM-related careers. Our ultimate goal is<br/>
+				to give anyone the possibility of becoming an innovator.<br/>
+			</p>
+			<img id="fresh" src="/res/img/IndiaDay.jpg"/>
+		</div>
+	</body>
+</html>
